734f55262aa1d10de9b31f8bb864803a587b00dd
Mikael Goldmann <migo@spotify.com>
2020-12-20 21:25:00 +0100

    New intcode version

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -2,7 +2,10 @@ import sys
 
 
 def read_prog(inp):
-    return [int(x) for x in inp.readline().strip().split(',')]
+    line = inp.readline()
+    while line:
+        yield [int(x) for x in line.strip().split(',')]
+        line = inp.readline()
 
 
 def val(mem, x, m):
@@ -15,46 +18,45 @@ def val(mem, x, m):
         
 
 
-def step(mem, pc, inp, out):
+def step(mem, pc, inp):
     xop = mem[pc]
     op = xop % 100
     m1 = (xop // 100) % 10
     m2 = (xop // 1000) % 10
     m3 = (xop // 10000) % 10
     assert m3 == 0
-
+    
     if op == 1:
         # add
         p1, p2, p3 = mem[pc+1 : pc+4]
         mem[p3] = val(mem, p1, m1) + val(mem, p2, m2)
-        return pc + 4
+        return pc + 4, None
     elif op == 2:
         # mul
         p1, p2, p3 = mem[pc+1 : pc+4]
         mem[p3] = val(mem, p1, m1) * val(mem, p2, m2)
-        return pc + 4
+        return pc + 4, None
     elif op == 3:
         # input
         a = mem[pc + 1]
         mem[a] = next(inp)
-        return pc + 2
+        return pc + 2, None
     elif op == 4:
         # output
         p1 = mem[pc + 1]
-        out.append(val(mem, p1, m1))
-        return pc + 2
+        return pc + 2, val(mem, p1, m1)
     elif op == 5:
         # jump-if-true
         p1, p2 = mem[pc+1: pc+3]
         if val(mem, p1, m1) != 0:
-            return val(mem, p2, m2)
-        return pc + 3
+            return val(mem, p2, m2), None
+        return pc + 3, None
     elif op == 6:
         # jump-if-false
         p1, p2 = mem[pc+1: pc+3]
         if val(mem, p1, m1) == 0:
-            return val(mem, p2, m2)
-        return pc + 3
+            return val(mem, p2, m2), None
+        return pc + 3, None
     elif op == 7:
         # less-than
         p1, p2, p3 = mem[pc+1 : pc+4]
@@ -62,7 +64,7 @@ def step(mem, pc, inp, out):
             mem[p3] = 1
         else:
             mem[p3] = 0
-        return pc + 4
+        return pc + 4, None
     elif op == 8:
         # equal-to
         p1, p2, p3 = mem[pc+1 : pc+4]
@@ -70,19 +72,27 @@ def step(mem, pc, inp, out):
             mem[p3] = 1
         else:
             mem[p3] = 0
-        return pc + 4
+        return pc + 4, None
+    elif op == 99:
+        return pc, None
     else:
         raise Exception("illegal op " + op)
 
 
-def run(mem, inp):
-    inp = iter(inp)
+
+def run(mem, inp, pc):
+    """
+    mem: initial memory config (parameter not modified)
+    inp: iterator of inputs
+    pc: start program counter
+
+    Runs one step
+    Returns: memory contents (internal state), output or None, next pc, bool running (False if last op was 99)
+    """
     out = []
     mem = mem[:]
-    pc = 0
     while mem[pc] != 99:
-        pc = step(mem, pc, inp, out)
-        # print(mem)
-    return mem, out
-
-mem = read_prog(sys.stdin)
+        pc, out = step(mem, pc, inp)
+        yield mem, out, pc, True
+    while True:
+        yield mem, None, pc, False
