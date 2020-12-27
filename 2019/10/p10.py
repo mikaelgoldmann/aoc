c98e8e800ad5ca10e49fb48193418360130d61c0
Mikael Goldmann <migo@spotify.com>
2020-12-27 14:39:09 +0100

    2019-12-10 part 2

--- 2019/10/p10.py
+++ 2019/10/p10.py
@@ -1,10 +1,11 @@
 import sys
+import math
+from collections import deque
 
-
-def read_puzzles():
+def read_puzzles(inp=sys.stdin):
     ps = []
     p = []
-    for line in sys.stdin:
+    for line in inp:
         line = line.strip()
         if not line:
             ps.append(p)
@@ -69,16 +70,81 @@ def solve1(p):
     nc = len(p[0])
     nr = len(p)
     mx = 0
+    mc, mr = -1, -1
     for r in range(nr):
         for c in range(nc):
             if p[r][c] == '#':
-                mx = max(mx, count_seen(r, c, p))
-    return mx
+                seen = count_seen(r, c, p)
+                if mx < seen:
+                    mx = seen
+                    mc = c
+                    mr = r
+    return mx, mr, mc
 
 
-puzzles = read_puzzles()
+with open(sys.argv[1]) as f:
+    puzzles = read_puzzles(f)
+
+
+def around(mr, mc):
+    def key(r, c):
+        assert r != mr or c != mc
+        dst2 = (mr - r)**2 + (mc - c)**2
+        if mc == c:
+            angle = 0 if r < mr else math.pi
+        elif r == mr:
+            angle = math.pi / 2 if c > mc else 3 * math.pi / 2
+        else:
+            alpha = math.atan(abs(c - mc) / abs(r - mr))
+            if r > mr:
+                if c < mc:  # upper right
+                    angle = alpha
+                else:  # lower right
+                    angle = math.pi - alpha
+            else:
+                if c > mc:  # lower left
+                    angle = math.pi + alpha
+                else:  # upper left
+                    angle = 2 * math.pi - alpha
+        return (angle, dst2)
+    return key
+
+
+def solve2(p, mr, mc):
+    points = []
+    for r in range(len(p)):
+        for c in range(len(p[0])):
+            if r == mr and c == mc:
+                continue
+            if p[r][c] == '#':
+                points.append((r, c))
+    key_fun = around(mr, mc)
+    points = sorted(points, key=lambda x: key_fun(x[0], x[1]))
+    last = None, None
+    last_key = (None, None)
+    num = 0
+    pts = deque()
+    while num < 200:
+        if not(pts):
+            pts = deque(points)
+            points = []
+            last_key = None, None
+        r, c = pts.popleft()
+        key = key_fun(r, c)
+        if last_key[0] == key[0]:
+            points.append((r, c))
+        else:
+            last = r, c
+            num += 1
+            print("step", num, "pos", c, r)
+        last_key = key
+    return last
 
 
 for p in puzzles:
     print()
-    print("part 1", solve1(p))
+    mx, mr, mc = solve1(p)
+    print(mr, mc)
+    print("part 1", mx)
+    r, c = solve2(p, mr, mc)
+    print("part 2", c * 100 + r)
