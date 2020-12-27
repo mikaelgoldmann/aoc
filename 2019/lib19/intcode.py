b830fe6e1b770225c244a26900f619683c391816
Mikael Goldmann <migo@spotify.com>
2020-12-27 16:53:46 +0100

    Some intcode refactor

--- 2019/lib19/intcode.py
+++ 2019/lib19/intcode.py
@@ -1,5 +1,22 @@
 import collections
 
+class OpCode(object):
+    ADD = 1
+    MUL = 2
+    INP = 3
+    OUT = 4
+    JPT = 5
+    JPF = 6
+    LTE = 7
+    EQU = 8
+    BSE = 9
+    STP = 99
+
+class Mode(object):
+    POS = 0
+    IMD = 1
+    REL = 2
+
 
 def read_prog(inp):
     line = inp.readline()
@@ -8,94 +25,99 @@ def read_prog(inp):
         line = inp.readline()
 
 
-def addr(mode, base, a):
-    if mode == 0:
+def addr(a, mode, base):
+    if mode == Mode.POS:
         return a
-    if mode == 2:
+    if mode == Mode.REL:
         return base + a
     raise Exception("Illegal mode: " + str(mode))
 
 
 def val(mem, x, m, base):
-    if m == 1:
+    if m == Mode.IMD:
         return x
-    elif m in [0, 2]:
-        return mem[addr(m, base, x)]
     else:
-        raise Exception("illegal mode [%d]" % m)
+        return mem[addr(x, m, base)]
 
 
-def fetch(mem, addrs):
-    return [mem[a] for a in addrs]
+def fetch_params(mem, pc, num):
+    return [mem[pc + 1 + i] for i in range(num)]
 
 
-def step(mem, pc, inp, base):
-    xop = mem[pc]
+def parse_op(xop):
     op = xop % 100
     m1 = (xop // 100) % 10
     m2 = (xop // 1000) % 10
     m3 = (xop // 10000) % 10
+    return op, (m1, m2, m3)
 
-    if op == 1:
-        # add
-        p1, p2, p3 = fetch(mem, [pc + 1, pc + 2, pc + 3])
-        mem[addr(m3, base, p3)] = val(mem, p1, m1, base) + val(mem, p2, m2, base)
-        return base, pc + 4, None
-    elif op == 2:
-        # mul
-        p1, p2, p3 = fetch(mem, [pc + 1, pc + 2, pc + 3])
-        mem[addr(m3, base, p3)] = val(mem, p1, m1, base) * val(mem, p2, m2, base)
-        return base, pc + 4, None
-    elif op == 3:
-        # input
-        p1, = fetch(mem, [pc + 1])
-        a = addr(m1, base, p1)
+
+def step(mem, pc, inp, base):
+    op, (m1, m2, m3) = parse_op(mem[pc])
+
+    if op == OpCode.ADD:
+        npars = 3
+        p1, p2, p3 = fetch_params(mem, pc, npars)
+        mem[addr(p3, m3, base)] = val(mem, p1, m1, base) + val(mem, p2, m2, base)
+        return base, pc + npars + 1, None
+    elif op == OpCode.MUL:
+        npars = 3
+        p1, p2, p3 = fetch_params(mem, pc, npars)
+        mem[addr(p3, m3, base)] = val(mem, p1, m1, base) * val(mem, p2, m2, base)
+        return base, pc + npars + 1, None
+    elif op == OpCode.INP:
+        npars = 1
+        p1, = fetch_params(mem, pc, npars)
+        a = addr(p1, m1, base)
         mem[a] = next(inp)
-        return base, pc + 2, None
-    elif op == 4:
-        # output
-        p1, = fetch(mem, [pc + 1])
+        return base, pc + npars + 1, None
+    elif op == OpCode.OUT:
+        npars = 1
+        p1, = fetch_params(mem, pc, npars)
         #        p1 = mem[addr(m1, base, pc + 1)]
-        return base, pc + 2, val(mem, p1, m1, base)
-    elif op == 5:
-        # jump-if-true
-        p1, p2 = fetch(mem, [pc + 1, pc + 2])
+        return base, pc + npars + 1, val(mem, p1, m1, base)
+    elif op == OpCode.JPT:
+        npars = 2
+        p1, p2 = fetch_params(mem, pc, npars)
         if val(mem, p1, m1, base) != 0:
             return base, val(mem, p2, m2, base), None
-        return base, pc + 3, None
-    elif op == 6:
-        # jump-if-false
-        p1, p2 = fetch(mem, [pc + 1, pc + 2])
+        return base, pc + npars + 1, None
+    elif op == OpCode.JPF:
+        npars = 2
+        p1, p2 = fetch_params(mem, pc, npars)
         if val(mem, p1, m1, base) == 0:
             return base, val(mem, p2, m2, base), None
-        return base, pc + 3, None
-    elif op == 7:
-        # less-than
-        p1, p2, p3 = fetch(mem, [pc + 1, pc + 2, pc + 3])
+        return base, pc + npars + 1, None
+    elif op == OpCode.LTE:
+        npars = 3
+        p1, p2, p3 = fetch_params(mem, pc, npars)
+        addr1 = addr(p3, m3, base)
         if val(mem, p1, m1, base) < val(mem, p2, m2, base):
-            mem[addr(m3, base, p3)] = 1
+            mem[addr1] = 1
         else:
-            mem[addr(m3, base, p3)] = 0
-        return base, pc + 4, None
-    elif op == 8:
-        # equal-to
-        p1, p2, p3 = fetch(mem, [pc + 1, pc + 2, pc + 3])
+            mem[addr1] = 0
+        return base, pc + npars + 1, None
+    elif op == OpCode.EQU:
+        npars = 3
+        p1, p2, p3 = fetch_params(mem, pc, npars)
+        addr2 = addr(p3, m3, base)
         if val(mem, p1, m1, base) == val(mem, p2, m2, base):
-            mem[addr(m3, base, p3)] = 1
+            mem[addr2] = 1
         else:
-            mem[addr(m3, base, p3)] = 0
-        return base, pc + 4, None
-    elif op == 9:
-        p1 = mem[pc + 1]
+            mem[addr2] = 0
+        return base, pc + npars + 1, None
+    elif op == OpCode.BSE:
+        npars = 1
+        p1, = fetch_params(mem, pc, npars)
         base += val(mem, p1, m1, base)
-        return base, pc + 2, None
+        return base, pc + npars + 1, None
     elif op == 99:
         return base, pc, None
     else:
         raise Exception("illegal op " + op)
 
 
-def run(prog, inp, pc):
+def run(prog, inp, pc=0):
     """
     mem: initial memory config (parameter not modified)
     inp: iterator of inputs
