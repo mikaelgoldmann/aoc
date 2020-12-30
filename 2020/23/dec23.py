b44544ae27a9d26d48809283caecbc13c07bd489
Mikael Goldmann <migo@spotify.com>
2020-12-30 20:19:09 +0100

    Dec 23 linked list version

--- 2020/23/dec23.py
+++ 2020/23/dec23.py
@@ -6,7 +6,7 @@ def rot(d):
     d.append(x)
 
 
-def step(d, n):
+def step(lst, n):
     def nxt(m):
         m -= 1
         if m < 1:
@@ -15,34 +15,83 @@ def step(d, n):
 
     # current pos is always the first pos!
     elts = []
-    current = d.popleft()
+    current = lst.current
     for i in range(3):
-        elts.append(d.popleft())
-    d.appendleft(current)
+        elt = lst.right(current)
+        lst.out(elt)
+        elts = [elt] + elts  # reverse order for easier insertion
     target = nxt(current)
     while target in elts:
         target = nxt(target)
-    while d[-1] != target:
-        rot(d)
-    d.extend(elts)
-    while d[-1] != current:
-        rot(d)
+    for e in elts:  # here it helps that elts are reversed
+        lst.insert_after(e, target)
+    lst.current = lst.right(current)
+
+
+class Link(object):
+    def __init__(self, me, lft, rte):
+        self.me = me
+        self.left = lft
+        self.right = rte
+
+    def out(self, links):
+        lft = links[self.left]
+        rte = links[self.right]
+        lft.right = self.right
+        rte.left = self.left
+        self.left = None
+        self.right = None
+
+    def insert_after(self, other, links):
+        assert self.left is None and self.right is None
+        lft = links[other]
+        rte = links[lft.right]
+        self.left = other
+        self.right = lft.right
+        lft.right = self.me
+        rte.left = self.me
+
+
+class List(object):
+    def __init__(self, current, links):
+        self.current = current
+        self.links = links
+
+    def out(self, name):
+        assert self.current != name
+        return self.links[name].out(self.links)
+
+    def insert_after(self, to_insert, after):
+        self.links[to_insert].insert_after(after, self.links)
+
+    def left(self, pos):
+        return self.links[pos].left
+
+    def right(self, pos):
+        return self.links[pos].right
 
 
 def main(data, steps):
-    data = deque([int(x) for x in data])
+    links = {}
+    data = [int(x) for x in data]
+    for i in range(len(data)):
+        links[data[i]] = Link(data[i], data[i-1], data[(i + 1) % len(data)])
+
     assert set(data) == set(range(1, 10))
     #print("Step", 0)
     #print(data)
+    my_list = List(data[0], links)
     for i in range(steps):
         #print(i)
-        step(data, 10)
+        step(my_list, 10)
         #print("Step", i + 1)
         #print(data)
-    while data[0] != 1:
-        rot(data)
-    result = map(str, data)
-    return ''.join(result)[1:]
+    pos = my_list.right(1)
+    result = []
+    while pos != 1:
+        result.append(str(pos))
+        pos = my_list.right(pos)
+    return ''.join(result)
 
 
 if __name__ == '__main__':
--- 2020/23/dec23test.py
+++ 2020/23/dec23test.py
@@ -4,6 +4,6 @@ from dec23 import main
 
 class Test(unittest.TestCase):
     def test(self):
-        self.assertEqual(main("389125467", 10), "92658374")
-        self.assertEqual(main("389125467", 100), "67384529")
-        self.assertEqual(main("789465123", 100), "98752463")
+        self.assertEqual("92658374", main("389125467", 10))
+        self.assertEqual("67384529", main("389125467", 100))
+        self.assertEqual("98752463", main("789465123", 100))
