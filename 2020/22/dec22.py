cf4274b65447c1b2db4182c97bc134eafa0b50c3
Mikael Goldmann <migo@spotify.com>
2020-12-30 18:02:06 +0100

    2020-12-22 part 2

--- 2020/22/dec22.py
+++ 2020/22/dec22.py
@@ -25,7 +25,9 @@ def play(d1, d2):
         d2.append(c1)
 
 
-def points(d):
+def points(d1, d2=None):
+    assert not d1 or not d2
+    d = deque(d1 or d2)
     mult = 1
     sum = 0
     while d:
@@ -41,18 +43,52 @@ def play_game_1(d1, d2):
     while dq1 and dq2:
         play(dq1, dq2)
     print(dq1, dq2)
-    pts = points(dq1) if d1 else points(dq2)
-    return pts
+    return dq1, dq2
+
+
+def round(dq1, dq2):
+    c1 = dq1.popleft()
+    c2 = dq2.popleft()
+    if c1 > len(dq1) or c2 > len(dq2):
+        # highest card wins
+        if c1 > c2:
+            dq1.extend([c1, c2])
+        else:
+            dq2.extend([c2, c1])
+    else:
+        # play recursive game
+        s1, s2 = play_game_2(list(dq1)[:c1], list(dq2)[:c2])
+        if s1:
+            dq1.extend([c1, c2])
+        else:
+            dq2.extend([c2, c1])
+
+
+def play_game_2(d1, d2):
+    # returns dq1 , dq2 which are the winners deque and the losers (empty) deque
+    dq1 = deque(d1)
+    dq2 = deque(d2)
+    seen = set()
+    while dq1 and dq2:
+        key = (tuple(dq1), tuple(dq2))
+        if key in seen:
+            return list(dq1), []
+        round(dq1, dq2)
+        seen.add(key)
+    return dq1, dq2
 
 
 def main(inp):
     d1 = read_deck(inp)
     d2 = read_deck(inp)
     assert len(d1) == len(d2)
-    print(d1, d2)
-    pts = play_game_1(d1, d2)
-    print(d1, d2)
+    # print(d1, d2)
+    pts = points(*play_game_1(d1, d2))
+    # print(d1, d2)
     print("part 1", pts)
+    s1, s2 = play_game_2(d1, d2)
+    pts = points(s1, s2)
+    print("part 2", pts)
 
 
 if __name__ == '__main__':
