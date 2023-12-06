607b7f2dd49f31f0e6b98df61faf620ad5a19275
Mikael Goldmann <migo@spotify.com>
2023-12-06 21:14:18 +0100

    Dec 03 2023 part 2

--- 2023/03/dec03.py
+++ 2023/03/dec03.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import sys
 
 
@@ -39,7 +40,6 @@ def get_int_lines(lines=None):
 
 
 def fill(board: list, nums: list, i:int , j: int):
-    print("fill", i, j)
     for di in range(-1, 2):
         for dj in range(-1, 2):
             try:
@@ -55,6 +55,18 @@ def fill(board: list, nums: list, i:int , j: int):
                         k += 1
             except IndexError:
                 pass
+
+
+def get_number(row: list, pos: int, num_row: list, gear_num: int) -> int:
+    assert row[pos].isdigit()
+    while pos - 1 >= 0 and row[pos - 1].isdigit():
+        pos -= 1
+    s = 0
+    while pos < len(row) and row[pos].isdigit():
+        s = s * 10 + ord(row[pos]) - ord('0')
+        num_row[pos] = gear_num
+        pos += 1
+    return s
 
 
 def get_numbers(brow, nrow):
@@ -73,6 +85,19 @@ def get_numbers(brow, nrow):
     return r
 
 
+def find_numbers_around(board: list, i: int, j: int, nums: list, gear_num: int) -> list:
+    nums_around = []
+    for di in range(-1, 2):
+        if i + di < 0 or i + di >= len(board):
+            continue
+        for dj in range(-1, 2):
+            if j + dj < 0 or j + dj >= len(board[0]):
+                continue
+            if board[i+di][j + dj].isdigit() and nums[i+di][j+dj] < gear_num:
+                nums_around.append(get_number(board[i + di], j + dj, nums[i + di], gear_num))
+    return nums_around
+
+
 def main(argv):
     board = get_lines()
 
@@ -88,7 +113,18 @@ def main(argv):
         s += sum(get_numbers(brow, nrow))
 
     print(s)
+
     print("part 2")
+    gear_lists = defaultdict(list)
+    gear_num = 0
+    nums = [[0 for j in range(len(board[0]))] for i in range(len(board))]
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == '*':
+                gear_num += 1
+                gear_lists[gear_num] = find_numbers_around(board, i, j, nums, gear_num)
+    print(sum(g[0] * g[1] for g in gear_lists.values() if len(g) == 2))
+
 
 
 if __name__ == '__main__':
