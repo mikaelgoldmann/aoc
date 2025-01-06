c3b58fca08ade79fa6af48b8e1ca52db42beaec9
Mikael Goldmann <migo@spotify.com>
2025-01-06 11:21:52 +0100

    2024-12-13 using lin alg

--- 2024/13/dec13.py
+++ 2024/13/dec13.py
@@ -1,6 +1,4 @@
-from collections import namedtuple
 from dataclasses import dataclass, field
-from heapq import heappop, heappush
 import argparse
 import re
 import sys
@@ -15,6 +13,13 @@ class Test:
     a: (int, int)
     b: (int, int)
     prize: (int, int)
+
+    def __str__(self):
+        return """
+        A: %d, %d
+        B: %d, %d
+        P: %d, %d
+        """ % (self.a[0], self.a[1], self.b[0], self.b[1], self.prize[0], self.prize[1])
 
 
 @dataclass(order=True)
@@ -66,7 +71,6 @@ def get_paragraphs(lines=None):
 
 
 def parse_button(line):
-    dprint('button', line)
     m = BUTTON.match(line)
     return int(m.group(1)), int(m.group(2))
 
@@ -84,41 +88,60 @@ def parse_test(lines):
     return Test(a, b, prize)
 
 
-def eval1(test_case):
+def eval2(test_case):
     xa, ya = test_case.a
     xb, yb = test_case.b
-    px, py = test_case.prize
-    q = [Node(0, 0, 0)]
-    seen = set()
-    while q:
-        node = heappop(q)
-        if node.is_goal(px, py):
-            return node.d
-        if node.coord() in seen:
-            continue
-        seen.add(node.coord())
-        push_a = node.add(xa, ya, 3)
-        if push_a.coord() not in seen and push_a.is_valid(px, py):
-            heappush(q, push_a)
-        push_b = node.add(xb, yb, 1)
-        if push_b.coord() not in seen and push_b.is_valid(px, py):
-            heappush(q, push_b)
-    return 0
+    xp, yp = test_case.prize
+    # / xa xb \  / u \  = / xp \
+    # \ ya yb /  \ v /    \ yp /
+    dprint(test_case)
+    det = (xa * yb) - (ya * xb)
+    if det == 0:
+        # degenerate case... zero or infinite solutions
+        # if infinite, find cheapest
+        dprint("singular")
+        raise NotImplemented("Singular matrix")
+    else:
+        # normal case, solve and check that it is a legal, integer solution
+        # inverse is
+        #
+        # det^(-1)  /  yb -xb \
+        #           \ -ya  xa /
+        dprint("non-singular")
+        u1 = yb * xp - xb * yp
+        v1 = xa * yp - ya * xp
+        dprint(u1 * xa,  v1 * xb, xp, det)
+        assert u1 * xa + v1 * xb == xp * det
+        dprint(u1, v1, det)
+        if u1 % det != 0:
+            return 0
+        if v1 % det != 0:
+            return 0
+        u = u1 // det
+        v = v1 // det
+        dprint(u, v)
+        if u < 0 or v < 0:
+            return 0
+        return 3 * u + v
 
-def solve1(test_cases):
+
+def solve2(test_cases):
     acc = 0
     for i, t in enumerate(test_cases):
-        acc += eval1(t)
-        print(i)
+        acc += eval2(t)
+        dprint(i)
     return acc
 
 
 def main(the_file):
     test_cases = [parse_test(p) for p in get_paragraphs(the_file)]
     print("part 1")
-    print(solve1(test_cases))
+    print(solve2(test_cases))
     print()
     print("part 2")
+    TERM = 10000000000000
+    test_cases2 = [Test(t.a, t.b, (t.prize[0] + TERM, t.prize[1] + TERM)) for t in test_cases]
+    print(solve2(test_cases2))
 
 
 if __name__ == '__main__':
@@ -127,6 +150,7 @@ if __name__ == '__main__':
     parser.add_argument("--file", default=None)
     args = parser.parse_args(sys.argv[1:])
     DEBUG = args.debug
+    print(DEBUG)
     if args.file:
         with open(args.file) as inp:
             main(inp)
