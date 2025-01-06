ffd3bda1865d81190fed3d60a965c3e384b80649
Mikael Goldmann <migo@spotify.com>
2025-01-06 20:40:09 +0100

    2024-12-15 part 1

--- 2024/15/dec15.py
+++ 2024/15/dec15.py
@@ -1,8 +1,25 @@
 import sys
 import argparse
-
+from copy import deepcopy
 
 DEBUG = False
+
+
+LEFT, RIGHT, UP, DOWN = list(iter("<>^v"))
+
+
+D = {
+    LEFT: (0, -1),
+    RIGHT: (0, 1),
+    UP: (-1, 0),
+    DOWN: (1, 0)
+}
+
+
+ROBOT = '@'
+SPACE = '.'
+BOX = 'O'
+BORDER = '#'
 
 
 def dprint(*a, **kw):
@@ -31,24 +48,54 @@ def get_paragraphs(lines=None):
         yield paragraph
 
 
-def get_int_lines(lines=None):
-    if lines is None:
-        lines = get_lines()
-    int_lines = []
-    for line in lines:
-        int_line = []
-        for x in line.split():
-            try:
-                x = int(x)
-                int_line.append(x)
-            except:
-                pass
-        int_lines.append(int_line)
-    return int_lines
+def parse_board(lines):
+    return [list(iter(line)) for line in lines]
+
+
+def parse_moves(lines):
+    return ''.join(lines)
+
+
+def step(board, r, c, move):
+    dr, dc = D[move]
+    r1, c1 = r + dr, c + dc
+    while board[r1][c1] == BOX:
+        r1, c1 = r1 + dr, c1 + dc
+    if board[r1][c1] == BORDER:
+        # cannot move
+        return r, c
+    r2, c2 = r + dr, c + dc
+    board[r1][c1] = board[r2][c2]
+    board[r][c] = SPACE
+    board[r2][c2] = ROBOT
+    return r2, c2
+
+
+def find_cells(board, value):
+    cells = []
+    for r, row in enumerate(board):
+        for c, cell in enumerate(row):
+            if cell == value:
+                cells.append((r, c))
+    return cells
+
+
+def solve1(board, moves):
+    robots = find_cells(board, ROBOT)
+    assert len(robots) == 1
+    r, c = robots[0]
+    for m in moves:
+        r, c = step(board, r, c, m)
+    boxes = find_cells(board, BOX)
+    return sum( r * 100 + c for r, c in boxes)
 
 
 def main(inp):
+    paragraphs = [p for p in get_paragraphs(inp)]
+    board = parse_board(paragraphs[0])
+    moves = parse_moves(paragraphs[1])
     print("part 1")
+    print(solve1(deepcopy(board), deepcopy(moves)))
     print()
     print("part 2")
 
@@ -56,11 +103,8 @@ def main(inp):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", type=bool, default=False)
-    parser.add_argument("--file", type=str, default=None)
-    args = parser.parse_args(sys.argv)
+    parser.add_argument("--file", type=str)
+    args = parser.parse_args(sys.argv[1:])
     DEBUG = args.debug
-    if args.file:
-        with open(args.file) as inp:
-            main(inp)
-    else:
-        main(sys.stdin)
+    with open(args.file) as inp:
+        main(inp)
